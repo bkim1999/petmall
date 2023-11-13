461c69050f1a9e01603e6d58f96cde1ce1e3f05e
dntjr7004 <dntjr7004@naver.com>
2023-11-13 11:35:26 +0900

    jws7

--- src/main/java/com/gdu/petmall/controller/AdminController.java
+++ src/main/java/com/gdu/petmall/controller/AdminController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.gdu.petmall.service.AdminService;
+import com.gdu.petmall.service.ProductService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,11 +15,13 @@ import lombok.RequiredArgsConstructor;
 public class AdminController {
   
   private final AdminService adminService;
-  
+  private final ProductService productService;
   
   @GetMapping("/admin.go")
   public String loadeventlist() {
     return "admin/list";
   }
+  
+  
 
 }
