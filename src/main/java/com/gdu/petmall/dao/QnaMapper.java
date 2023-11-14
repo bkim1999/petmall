e45fa900ed48aa133b2009520e05c7a19f9684a5
kangminjunes <1999koco@naver.com>
2023-11-14 10:46:34 +0900

    mj1114

--- src/main/java/com/gdu/petmall/dao/QnaMapper.java
+++ src/main/java/com/gdu/petmall/dao/QnaMapper.java
@@ -14,7 +14,6 @@ public interface QnaMapper {
    public int insertQna(QnaDto qna);
    public int insertQattach(QattachDto qattach);
    
-   public int getQnaCount();
    public List<QnaDto> getQnaList(Map<String, Object> map);
     public List<QnaDto> getMyPostList(Map<String, Object> paramMap);
     
@@ -26,4 +25,5 @@ public interface QnaMapper {
     
     public List<QnaDto> getAllQnalist();
     public int getQnaCount(int checkFlag);
+    public int qnaTotalCount();
 }
